1660983b3ca7b9e71018d3c8d8a8bde521a2b436
MiguelSMaciel <106818945+MiguelSMaciel@users.noreply.github.com>
2022-12-08 11:46:50 -0300

    SceneLoading

--- Assets/Scripts/Miguel/LevelLoader.cs
+++ Assets/Scripts/Miguel/LevelLoader.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TMP_Text progressText;
     public void LoadLevel(int sceneIndex)
     {
         StartCoroutine(LoadAsynChronosly(sceneIndex));
@@ -14,9 +19,14 @@ public class LevelLoader : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScreen.SetActive(true);
+
         while (!operation.isDone)
         {
-            Debug.Log(operation.isDone);
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
 
             yield return null;
         }
